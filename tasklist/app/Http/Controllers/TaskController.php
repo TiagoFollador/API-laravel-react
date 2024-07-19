fd7206be58ab1b5015d4f6a6ac8da86c72ea8086
estagiario-dev2 <estagiario-dev2@escape79.com.br>
2024-07-19 14:36:56 -0300

    The Frontend: How to Install the Packages, foi onde parei

--- tasklist/app/Http/Controllers/TaskController.php
+++ tasklist/app/Http/Controllers/TaskController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Task\CreateTaskRequest;
 use App\Http\Requests\Task\ListTaskRequest;
+use App\Http\Requests\Task\ReorderTaskRequest;
+use App\Http\Requests\Task\UpdateTaskRequest;
 use App\Services\ProjectService;
 use App\Services\TaskService;
 use Illuminate\Http\JsonResponse;
@@ -29,6 +32,40 @@ class TaskController extends Controller
             'sucess' => true,
             'tasks' => $tasks,
             'message' => "Tasks retrived succesfully",
-        ]);
+        ]); // 200
     }
+
+    public function store(CreateTaskRequest $request): JsonResponse
+    {
+        $this->taskService->store($request->all());
+
+        return response()->json([
+            'sucess' => true,
+            'message' => "Task created succesfully"
+        ], 201);
+    }
+
+    public function update(UpdateTaskRequest $request, int $id): JsonResponse
+    {
+        $this->taskService->update($id, $request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => "Task update successfully",
+        ], 201);
+    }
+
+    public function reorder(ReorderTaskRequest $request): JsonResponse
+    {
+        $this->taskService->reorder(
+            $request->get('project_id'),
+            $request->get('start'),
+            $request->get('end'),
+        );
+
+        return response()->json([
+            'success' => true,
+            'message' => "Tasks reordered successfully",
+        ], 201);
+    } 
 }
